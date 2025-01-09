1074e3e5a96fa2c5df1c117d9e0013ad0aae408f
Raimond <raimondolle@gmail.com>
2025-01-09 11:09:16 +0200

    adding client module

---
+++ client-for-apcminideck/src/test/java/com/raimond/clientforapcminideck/ClientForApcminideckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimond.clientforapcminideck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientForApcminideckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
