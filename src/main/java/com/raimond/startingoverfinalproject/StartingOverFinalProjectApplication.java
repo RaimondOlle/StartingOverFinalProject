3e6ca6e48b50b1e41988ae4361d766358ccbf004
Raimond <raimondolle@gmail.com>
2025-01-09 08:24:37 +0200

    First commit

---
+++ src/main/java/com/raimond/startingoverfinalproject/StartingOverFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.raimond.startingoverfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingOverFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingOverFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raimond/startingoverfinalproject/StartingOverFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimond.startingoverfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingOverFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
