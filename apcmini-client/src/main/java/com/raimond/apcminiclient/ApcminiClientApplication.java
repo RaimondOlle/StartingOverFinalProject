79e1ba208772065b4bb46b8777ec8b38b93466f1
Raimond <raimondolle@gmail.com>
2025-01-09 11:29:25 +0200

    First commit in new-client

---
+++ apcmini-client/src/main/java/com/raimond/apcminiclient/ApcminiClientApplication.java
@@ -0,0 +1,13 @@
+package com.raimond.apcminiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApcminiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApcminiClientApplication.class, args);
+	}
+
+}
---
+++ apcmini-client/src/test/java/com/raimond/apcminiclient/ApcminiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimond.apcminiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcminiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
